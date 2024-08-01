4b25367ff7d9ae56aeb5c849d7248c0e372b2b29
Francis Addae <franco09e@yahoo.com>
2024-08-01 09:11:42 -0400

    making changes

--- data_flow/V3.7.0__schema.sql
+++ data_flow/V3.7.0__schema.sql
@@ -1,5 +1,5 @@
 -- This file will create the databse and schema and will specify which schema to use
-CREATE DATABASE IF NOT EXISTS francisDB;
+-- CREATE DATABASE IF NOT EXISTS francisDB;
 
 CREATE SCHEMA IF NOT EXISTS francisDB.present;
 
