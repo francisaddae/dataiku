53cccbc6faeb8ccfc1baa728cf8c8236c01bb7b7
Francis Addae <franco09e@yahoo.com>
2024-08-01 00:48:32 -0400

    add in new model changes

--- data_flow/schema.sql
+++
@@ -1,6 +0,0 @@
--- This file will create the databse and schema and will specify which schema to use
-CREATE DATABASE IF NOT EXISTS francisDB;
-CREATE SCHEMA IF NOT EXISTS francisDB.present;
-
-USE SCHEMA francisDB.present;
-
